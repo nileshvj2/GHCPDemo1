a469016bff3bdf630968cd51a7956dc7a5858bd9
nileshvj2 <nileshjoshi@microsoft.com>
2025-10-29 12:30:33 -0500

    saved

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
 print("hello world")
+
+print("hello hello")
+
+#function to add two numbers
+def add(num1, num2):
+    return num1 + num2
+
+# Main execution
+if __name__ == "__main__":
+    result = add(5, 3)
+    print("The sum is:", result)
+
+
+print("This is a new line added to the code.")
+print("Another new line added for testing.")
+
+#add function to multiply two numbers
+def multiply(num1, num2):
+    return num1 * num2
